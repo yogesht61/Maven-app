167c2c26adb663e456b1ad1c014d1b89941f30c4
yogesht61 <yogesht61@gmail.com>
2024-07-08 22:02:05 +0530

    first commit

---
+++ src/main/java/com/yogesh/app/App.java
@@ -0,0 +1,13 @@
+package com.yogesh.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
